5e1ccb23f2fafff8d0f089bbfa42c04bbcd3fbe5
Nafibal <baliya363@gmail.com>
2023-02-02 10:08:46 +0700

    finished service page template

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -33,6 +33,24 @@ const page = () => {
           </div>
         </div>
       </section>
+      <section className={styles.private_office_container}>
+        <h1>OUR Private Office</h1>
+        <div className={styles.office}>
+          <div className={styles.description}>
+            <div className={styles.top}>
+              <h2>Kelapa Gading</h2>
+              <p>Private Office with the following :</p>
+            </div>
+            <div className={styles.bottom}>
+              <h3>
+                Price : <span>Rp. 8.000.000,00</span>
+              </h3>
+              <button>Contact Us</button>
+            </div>
+          </div>
+          <div className={styles.image}></div>
+        </div>
+      </section>
     </main>
   );
 };
