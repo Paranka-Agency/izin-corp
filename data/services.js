7971d43d3d2a3daa2d51d24e3340447c9243ded1
Nafibal <baliya363@gmail.com>
2023-02-08 17:07:37 +0700

    create data

---
+++ data/services.js
@@ -0,0 +1,456 @@
+const services = [
+  {
+    id: "SR01",
+    title: "Spesialis Perizinan",
+    slug: "spesialis-perizinan",
+    desc: "Lorem ipsum dolor sit amet consectetur. Sit tellus nisl morbi sit ullamcorper diam. Neque dictum fermentum turpis in ornare suspendisse sed elit. Fusce quam sit duis purus curabitur donec porttitor tortor pharetra.",
+    service: [
+      {
+        id: "SR01-001",
+        title: "HGU - HGB - SHM / HakPakai / Hak pengelolaan",
+        slug: "hgu-hgb-shm",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image: "",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR01-002",
+        title: "Perkebunan",
+        slug: "perkebunan",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image:
+          "https://images.pexels.com/photos/2582652/pexels-photo-2582652.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR01-003",
+        title: "Pertambangan",
+        slug: "pertambangan",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image:
+          "https://images.pexels.com/photos/2892618/pexels-photo-2892618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR01-004",
+        title: "Kehutanan",
+        slug: "kehutanan",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image:
+          "https://images.pexels.com/photos/4827/nature-forest-trees-fog.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR01-005",
+        title: "Kepelabuhan",
+        slug: "kepelabuhan",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image:
+          "https://images.pexels.com/photos/237289/pexels-photo-237289.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR01-006",
+        title: "Perdagangan Ekspor dan Impor",
+        slug: "ekspor-impor",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image: "",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR01-007",
+        title: "Perumahan",
+        slug: "perumahan",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image:
+          "https://images.pexels.com/photos/1546168/pexels-photo-1546168.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR01-008",
+        title: "HAKI, Merk dagang",
+        slug: "haki",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image: "",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+    ],
+  },
+  {
+    id: "SR02",
+    title: "Spesialis Bisnis",
+    slug: "spesialis-bisnis",
+    desc: "Lorem ipsum dolor sit amet consectetur. Sit tellus nisl morbi sit ullamcorper diam. Neque dictum fermentum turpis in ornare suspendisse sed elit. Fusce quam sit duis purus curabitur donec porttitor tortor pharetra.",
+    service: [
+      {
+        id: "SR02-001",
+        title: "Konsultasi Bisnis",
+        slug: "konsultasi-bisnis",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image: "",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR02-002",
+        title: "Pendirian dan perubahan badan usaha",
+        slug: "pendirian-badan-usaha",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image: "",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR02-003",
+        title: "Perpajakan",
+        slug: "perpajakan",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image:
+          "https://images.pexels.com/photos/4386369/pexels-photo-4386369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR02-004",
+        title: "Badan Usaha dan perorangan",
+        slug: "badan-usaha-perseorangan",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image: "",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+    ],
+  },
+  {
+    id: "SR03",
+    title: "Spesialis Legal",
+    slug: "spesialis-legal",
+    desc: "Lorem ipsum dolor sit amet consectetur. Sit tellus nisl morbi sit ullamcorper diam. Neque dictum fermentum turpis in ornare suspendisse sed elit. Fusce quam sit duis purus curabitur donec porttitor tortor pharetra.",
+    service: [
+      {
+        id: "SR03-001",
+        title: "Konsultasi Hukum",
+        slug: "konsultasi-hukum",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image: "",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR03-002",
+        title: "Legal Corporate",
+        slug: "legal-corporate",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image:
+          "https://images.pexels.com/photos/159832/justice-law-case-hearing-159832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+      {
+        id: "SR03-003",
+        title: "Kontrak dan Perjanjian",
+        slug: "kontrak-perjanjian",
+        desc: "Lorem ipsum dolor sit amet consectetur. Nec convallis quis aenean sed. Lacus lorem senectus cras aenean sit dui. Amet amet viverra sollicitudin tortor. In aenean morbi id turpis mauris ac augue eleifend scelerisque. Nibh est id eget quis augue egestas justo sem volutpat. Dui purus lectus eu pretium amet nam.",
+        image:
+          "https://images.pexels.com/photos/261621/pexels-photo-261621.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        information: {
+          sp: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          hl: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+          ad: [
+            {
+              title: "Syaratnya",
+              desc: "Penjelasan dari syaratnya",
+            },
+          ],
+        },
+        price: 300000,
+      },
+    ],
+  },
+];
+
+export default services;
