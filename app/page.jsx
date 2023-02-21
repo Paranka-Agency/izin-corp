97dc58436a38e7f7a6ce45a6ad8fc3900e196cc9
Nafibal <baliya363@gmail.com>
2023-02-21 11:34:54 +0700

    convert scss modules to pure scss

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,26 @@
 "use client";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "./page.module.scss";
+import "./page.scss";
 import ServiceCard from "./comp/ServiceCard";
 import ReviewCard from "./comp/ReviewCard";
 import services from "@/data/services";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
+import useLocoScroll from "./hooks/useLocoScroll";
+import Footer from "./comp/Footer";
 
 export default function Home() {
+  useLocoScroll(true, "#home-container", "page_is-reveal");
+
   return (
-    <div className={styles.main}>
-      <section className={styles.hero} data-scroll-section>
+    <div className="main" id="home-container" data-scroll-container>
+      <section className="hero" data-scroll-section>
         <h1>IZIN CORP</h1>
       </section>
-      <section className={styles.about} data-scroll-section>
-        <div className={styles.container}>
-          <h1>SEKILAS TENTANG KAMI</h1>
+      <section className="about" data-scroll-section>
+        <div className="container">
+          <h1 data-scroll>SEKILAS TENTANG KAMI</h1>
           <p>
             IZIN CORP merupakan Konsultan berbadan hukum yang berfokus menangani
             perizinan berusaha, manajemen bisnis, dan perpajakan badan usaha
@@ -27,30 +29,31 @@ export default function Home() {
           <Link prefetch={false} href={"/about"}>
             <button>Lihat Selengkapnya</button>
           </Link>
-          <div className={`${styles.image} ${styles.left}`}></div>
-          <div className={`${styles.image} ${styles.right}`}></div>
+          <div className="image left"></div>
+          <div className="image right"></div>
         </div>
       </section>
-      <section className={styles.service} data-scroll-section>
-        <div className={styles.container}>
+      <section className="service" data-scroll-section>
+        <div className="container">
           <h2>We are here to give you special service.</h2>
-          <div className={styles.list}>
+          <div className="list">
             {services.map((service) => (
               <ServiceCard data={service} id={service.id} key={service.id} />
             ))}
           </div>
         </div>
       </section>
-      <section className={styles.review} data-scroll-section>
-        <div className={styles.container}>
+      <section className="review" data-scroll-section>
+        <div className="container">
           <h2>Apa pendapat orang lain tentang kami?</h2>
-          <div className={styles.card_container}>
+          <div className="card_container">
             <ReviewCard />
             <ReviewCard />
             <ReviewCard />
           </div>
         </div>
       </section>
+      <Footer data-scroll-section />
     </div>
   );
 }
